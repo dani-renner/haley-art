edea067aec06fab9ce8b8ad26531b816fdd877bf
Dani Renner <danijrenner@gmail.com>
2021-05-07 17:18:01 -0700

    combine reducer to rootReducer

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import formVisibleReducer from "./form-visible-reducer";
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer
+});
+
+export default rootReducer;
