49c20c547e8e67eba77272f996dd42f86cefd188
Dani Renner <danijrenner@gmail.com>
2021-05-14 15:46:14 -0700

    add input form for admin

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='title'
+          placeholder='Title of Piece:' />
+        <input
+          type='text'
+          name='medium'
+          placeholder='Medium(s) used:' />
+        <input
+          type='date'
+          name='dateMade'
+          placeholder='Date Completed:' />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
