24b0d224580fa80f19a38a563d1f76d6198bc1f3
Dani Renner <danijrenner@gmail.com>
2021-05-14 16:53:15 -0700

    add firebase to ArtList

--- src/components/ArtList.js
+++ src/components/ArtList.js
@@ -1,29 +1,41 @@
 import React from "react";
 import Art from "./Art";
 import PropTypes from "prop-types";
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
 function ArtList(props) {
-  return (
-    <React.Fragment>
-      {/* <hr /> */}
-      <h1><i>POST LIST</i></h1>
-      <hr></hr>
-      {Object.values(props.artList).map((art) =>
-        <Art
-          whenArtClicked={props.onArtSelection}
-          title={art.title}
-          medium={art.medium}
-          dateMade={art.dateMade}
-          id={art.id}
-          key={art.id}
-        />
-      )}
+  useFirestoreConnect([
+    { collection: 'art' }
+  ]);
+  const art = useSelector(state => state.firestore.ordered.art);
+  if (isLoaded(art)) {
+    return (
+      <React.Fragment>
+        {/* <hr /> */}
+        <h1><i>POST LIST</i></h1>
+        <hr></hr>
+        {Object.values(props.artList).map((art) =>
+          <Art
+            whenArtClicked={props.onArtSelection}
+            title={art.title}
+            medium={art.medium}
+            dateMade={art.dateMade}
+            id={art.id}
+            key={art.id}/>
+        )}
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+      <h3>Loading...</h3>
     </React.Fragment>
-  );
+    )
+  }
 }
 
 ArtList.propTypes = {
-  artList: PropTypes.object,
   onArtSelection: PropTypes.func
 };
 
