2a6badd1673642a83bc3793d6407e8f1b5a3c500
Dani Renner <danijrenner@gmail.com>
2021-05-16 15:52:41 -0700

    import artListReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import formVisibleReducer from "./form-visible-reducer";
+import artListReducer from "./art-list-reducer";
 import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
