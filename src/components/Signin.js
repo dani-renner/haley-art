d67b6e3ebf03a5e4a333c2a8cc96827e55bd8d10
Dani Renner <danijrenner@gmail.com>
2021-05-16 15:17:00 -0700

    add export and react import

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,37 +1,42 @@
+import React from "react";
 import firebase from "firebase/app";
 
-function doSignIn(event) {
-  event.preventDefault();
-  const email = event.target.signinEmail.value;
-  const password = event.target.signinPassword.value;
-  firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
-    console.log("Successfully signed in");
-  }).catch(function(error) {
-    console.log(error.message);
-  });
+function Signin(){
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+      console.log("Successfully signed in");
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+
+  function doSignOut() {
+    firebase.auth().signOut().then(function() {
+      console.log("Successfully signed out");
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+
+  <React.Fragment>
+    <h1>Sign In</h1>
+    <form onSubmit={doSignIn}>
+      <input
+        type='text'
+        name='signinEmail'
+        placeholder='email' />
+      <input
+        type='password'
+        name='signinPassword'
+        placeholder='Password' />
+      <button type='submit'>Sign in</button>
+    </form>
+    <h1>Sign Out</h1>
+    <button onClick={doSignOut}>Sign out</button>
+  </React.Fragment>
 }
 
-function doSignOut() {
-  firebase.auth().signOut().then(function() {
-    console.log("Successfully signed out");
-  }).catch(function(error) {
-    console.log(error.message);
-  });
-}
-
-<React.Fragment>
-  <h1>Sign In</h1>
-  <form onSubmit={doSignIn}>
-    <input
-      type='text'
-      name='signinEmail'
-      placeholder='email' />
-    <input
-      type='password'
-      name='signinPassword'
-      placeholder='Password' />
-    <button type='submit'>Sign in</button>
-  </form>
-  <h1>Sign Out</h1>
-  <button onClick={doSignOut}>Sign out</button>
-</React.Fragment>
+export default Signin
