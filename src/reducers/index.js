eff68e2d53c293ff125cf9336bf9b85f14bc5a2a
Dani Renner <danijrenner@gmail.com>
2021-05-16 15:25:42 -0700

    add firestore reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import formVisibleReducer from "./form-visible-reducer";
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
-  formVisibleOnPage: formVisibleReducer
+  formVisibleOnPage: formVisibleReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
