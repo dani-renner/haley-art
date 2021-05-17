eb28d3c837d7fc22be84a0af9d9641c9292c7f02
Dani Renner <danijrenner@gmail.com>
2021-05-16 19:32:00 -0700

    add onClickingDelete as prop

--- src/components/ArtDetail.js
+++ src/components/ArtDetail.js
@@ -2,17 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ArtDetail(props) {
-  const { art } =props;
+  const { art, onClickingDelete } =props;
 
   return (
     <React.Fragment>
       <hr />
+      <h3>{art.title}</h3>
     </React.Fragment>
   );
 }
 
 ArtDetail.PropTypes = {
-  art: PropTypes.object
+  art: PropTypes.object,
+  onClickingDelete: PropTypes.func
 }
 
 export default ArtDetail;
