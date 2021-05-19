7ae73412cacb1e6adcd958c12be7aa77857fae58
Dani Renner <danijrenner@gmail.com>
2021-05-18 20:59:51 -0700

    add admin route

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Header from './Header';
 import ArtControl from "./ArtControl";
+import Admin from './Admin';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
+    <React.Fragment>
     <Router>
-      <Header />
+    <Header />
+      <Switch>
+        <Route path="/admin">
+          <Admin />
+        </Route>
+      </Switch>
       <ArtControl />
     </Router>
+    </React.Fragment>
   );
 }
 
