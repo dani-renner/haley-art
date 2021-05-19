30dcdd978117553af0220ec93d7a28304b2cbe10
Dani Renner <danijrenner@gmail.com>
2021-05-19 09:24:08 -0700

    add route to sign in

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import ArtControl from "./ArtControl";
 import Admin from './Admin';
+import Signin from './Signin';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './../App.css';
 
@@ -13,6 +14,11 @@ function App() {
       <Switch>
         <Route path="/admin">
           <Admin />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/signin">
+          <Signin />
         </Route>
       </Switch>
       <ArtControl />
