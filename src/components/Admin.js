800ffe977faf324260211867b48154c435d354e1
Dani Renner <danijrenner@gmail.com>
2021-05-18 20:57:49 -0700

    add Admin component

---
+++ src/components/Admin.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Signin from "./Signin";
+
+export default function Admin(){
+  return (
+    <React.Fragment>
+      <p>Only the site owner can access</p>
+    </React.Fragment>
+  );
+}
