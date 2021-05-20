24bd8077eb5020173a88aaabb95fde2662264fd8
Dani Renner <danijrenner@gmail.com>
2021-05-20 00:58:57 -0700

    add MDB for styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,31 @@ import Header from './Header';
 import ArtControl from "./ArtControl";
 import Admin from './Admin';
 import Signin from './Signin';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import { MDBContainer } from 'mdb-react-ui-kit';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './../App.css';
 
 function App() {
   return (
-    <React.Fragment>
-    <Router>
-    <Header />
-      <Switch>
-        <Route path="/admin">
-          <Admin />
-        </Route>
-      </Switch>
-      <Switch>
-        <Route path="/signin">
-          <Signin />
-        </Route>
-      </Switch>
-      <ArtControl />
-    </Router>
-    </React.Fragment>
+    <MDBContainer>
+      <React.Fragment>
+      <Router>
+      <Header />
+        <Switch>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path="/signin">
+            <Signin />
+          </Route>
+        </Switch>
+        <ArtControl />
+      </Router>
+      </React.Fragment>
+    </MDBContainer>
   );
 }
 
